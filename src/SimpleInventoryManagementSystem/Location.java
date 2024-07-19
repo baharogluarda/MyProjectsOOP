1e7fd959b80d89322174e7ee1d6ff8ad05750495
Arda Baharoğlu <arda.900bhrgl@gmail.com>
2024-07-19 15:38:03 +0300

    My Projects

---
+++ src/SimpleInventoryManagementSystem/Location.java
@@ -0,0 +1,28 @@
+package SimpleInventoryManagementSystem;
+
+public class Location {
+    private String city;
+    private String country;
+
+    public Location(String city, String country) {
+        this.city = city;
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
+}
